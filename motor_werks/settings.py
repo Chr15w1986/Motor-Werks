aed5d095fa7c25ebe4ab5a0cafb4e3c48dd51b42
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-05 06:08:55 +0000

    fix error  with email_password

--- motor_werks/settings.py
+++ motor_werks/settings.py
@@ -118,14 +118,14 @@ AUTHENTICATION_BACKENDS = [
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com',
+    DEFAULT_FROM_EMAIL = 'motorwerks@motorwerks.com',
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
