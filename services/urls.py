acbe3c9349cbd9d10f900e094dd9ec878a964c68
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-12 12:49:31 +0000

    update service app to retrieve objects from the database

--- services/urls.py
+++ services/urls.py
@@ -3,10 +3,7 @@ from . import views
 
 APP_NAME = 'services'
 urlpatterns = [
-    path('', views.ServicesView.as_view(), name='services'),
-    path('major/', views.ServicesView.as_view(), name='major'),
-    path('minor/', views.ServicesView.as_view(), name='minor'),
-    path('suspension/', views.ServicesView.as_view(), name='suspension'),
-    path('brakes/', views.ServicesView.as_view(), name='brakes'),
-    path('tyres/', views.ServicesView.as_view(), name='tyres'),
+    path('', views.AllServicesView.as_view(), name='services'),
+    path('services_detail/<int:pk>/', views.SingleServiceDetail.as_view(),
+         name='services-detail'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic.list import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 from .models import Services
 
 
-class ServicesView(ListView):
+# class ServicesView(ListView):
+#     model = Services
+#     template_name = 'services/<int:pk>/'
+#     context_object_name = 'services'
+#     context = {'services/<int:pk>': Services}
+#     paginate_by = 1
+
+# class SingleServiceView(DetailView):
+#     model = Services
+#     template_name = 'services/'
+#     context = {'services/<int:pk>': Services}
+
+
+class AllServicesView(ListView):
+    """ Service list view for all available services"""
     model = Services
-    template_name = 'services/major.html'
-    success_url = reverse_lazy('major')
+    template_name = 'services/services.html'
     context_object_name = 'services'
+    queryset = Services.objects.all().order_by('service_name')
+    paginate_by = 5
+
+
+class SingleServiceDetail(DetailView):
+    """ SingleService view to allow users to view the
+        individual service chosen in more detail"""
+    model = Services
+
+    def services_detail(self, request, service_id):
+
+        services = get_object_or_404(Services, pk=service_id)
+
+        context = {
+            'services': services,
+        }
+
+        return render(request, 'services/services_detail.html', context)
