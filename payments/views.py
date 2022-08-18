c7381992a2aff6847a8748e006c2e33264eb0f72
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-18 13:06:44 +0000

    update primary key request

--- payments/views.py
+++ payments/views.py
@@ -32,7 +32,7 @@ def stripe_config(request):
 
 @csrf_exempt
 # Taken from Testdriven.io
-def create_checkout_session(request, pk):
+def create_checkout_session(request, pk=1):
 
     if request.method == 'GET':
         domain_url = settings.DOMAIN_URL
