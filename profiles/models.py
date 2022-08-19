683c282de826e3ec7925006f4c45908bdf13a237
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-19 14:52:31 +0000

    create profile form and link to models

--- profiles/models.py
+++ profiles/models.py
@@ -12,4 +12,4 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username}'
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,35 @@
 """ Profile app views """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileUpdateForm
 
 
 class ProfileView(TemplateView):
     template_name = 'profiles/profile.html'
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'{"Your account has been updated!"}')
+            return redirect('profiles/profile.html')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profiles/profile.html', context)
