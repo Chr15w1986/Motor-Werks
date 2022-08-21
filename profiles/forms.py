515cd95742a9def387c03218c4977338111808f8
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-21 17:43:25 +0000

    update profile app, add user info to form

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import UserProfile
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class UserForm(forms.ModelForm):
@@ -17,14 +19,8 @@ class UserForm(forms.ModelForm):
                   'postcode',
                   'phone_number')
 
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['user',
-                  'street_address1',
-                  'street_address2',
-                  'town_or_city',
-                  'county',
-                  'postcode',
-                  'phone_number']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'update-profile'))
--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,25 @@
 """ Profile app views """
-from django.shortcuts import render
+
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import UserForm
+from .models import UserProfile
 
 
 class ProfileView(TemplateView):
     template_name = 'profiles/profile.html'
 
 
-@login_required
-def userpage(request):
-    user_form = UserForm(instance=request.user)
+class UpdateProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
-    return render(
-                  request=request, template_name="profiles/profile.html",
-                  context={"user": request.user, "user_form": user_form, }
-        )
+    model = UserProfile
+    form = UserForm
+    template_name = 'profiles/update-profile.html'
+    success_url = reverse_lazy('update-profile')
+    context = {'form': form}
+
+    def test_func(self):
+        self.object = self.get_object()
+        return self.object.author == self.request.user
