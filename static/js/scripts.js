7f2174b2d97113c9607ad1f998507c00c5a1f2e6
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-08-29 12:37:51 +0000

    remove if statement causing console errors

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -16,7 +16,6 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
-
     };
 
     // Shrink the navbar 
@@ -28,10 +27,6 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
     };
 
     // Collapse responsive navbar when toggler is visible
