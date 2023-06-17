479ae323acd5da7d91e3aafaae861a6fd471aa75
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-17 19:33:47 +0000

    rework profile view to fix 500 error

--- profiles/views.py
+++ profiles/views.py
@@ -6,57 +6,44 @@ from django.contrib.auth import get_user_model
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 from .forms import UserForm
 from .models import UserProfile
 from services.models import ServiceHistory
 
-
-user = get_user_model()
+User = get_user_model()
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     """ Renders the profile in a form """
-    model = UserProfile
-    form = UserForm
     template_name = 'profiles/profile.html'
-    context = {'form': form}
-
-    def form_valid(self, form):
-        form.instance.booked_by = self.request.user
-        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['history'] = ServiceHistory.objects.filter(booked_by=self.request.user)  # noqa
-        context['form'] = UserForm(instance=self.request.user.userprofile)
+        context['user'] = self.request.user
+        context['history'] = ServiceHistory.objects.filter(booked_by=self.request.user) # noqa
         return context
 
 
 class UpdateProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """ Allows user to update personal profile
-        via a pop up modal"""
+        via a pop-up modal"""
     model = UserProfile
-    fields = ('first_name',
-              'last_name',
-              'email',
-              'vehicle_reg',
-              'street_address1',
-              'street_address2',
-              'town_or_city',
-              'county',
-              'postcode',
-              'phone_number')
+    form_class = UserForm
     template_name = 'profiles/profile.html'
 
-    def post(self, request, pk, *args, **kwargs):
-        user = request.user.userprofile.id
-        user_form = UserForm(request.POST, instance=request.user.userprofile)
+    def post(self, request, *args, **kwargs):
+        user_form = self.get_form()
         if user_form.is_valid():
-            user_form.instance.user = request.user
-            user_form.save()
-        return HttpResponseRedirect(reverse('update-profile', args=[user]))
+            return self.form_valid(user_form)
+        else:
+            return self.form_invalid(user_form)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('profile')
 
     def test_func(self):
         self.object = self.get_object()
@@ -68,11 +55,13 @@ class DeleteProfile(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = User
     template_name = 'profiles/delete-profile.html'
 
-    def post(self, request, pk, *args, **kwargs):
-        user = get_object_or_404(User, pk=request.user.id)
+    def post(self, request, *args, **kwargs):
+        user = self.get_object()
         user.delete()
-
         return HttpResponseRedirect(reverse('delete-success'))
+
+    def get_success_url(self):
+        return reverse('delete-success')
 
     def test_func(self):
         self.object = self.get_object()
@@ -82,5 +71,4 @@ class DeleteProfile(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 class DeleteSuccess(TemplateView):
     """ Renders a template to show user the deletion
         has been successful """
-    model = User
     template_name = 'profiles/delete-success.html'
