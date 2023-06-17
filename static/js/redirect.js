c7353263e03f2c65e687634dd03b472c0807d673
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-17 18:53:11 +0000

    update js script as previous version didnt work correctly

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -1,25 +1,21 @@
 // Simple timed redirect if the user does not click the home button
 // Set to 5 seconds
 
+var countdownElement = document.getElementById('countdown');
+var countdownTime = 5;
+countdownElement.innerText = 'Redirecting in: ' + countdownTime;
+
 setTimeout(function() {
     window.location.href = "/";
 }, 5000); // Redirect after 5 seconds (5000 milliseconds)
 
-// Countdown timer as a visual aid to the user
-var countdownElement = document.getElementById('countdown');
-var countdownTime = 5 * 60; // 5 minutes in seconds
 var countdownInterval = setInterval(updateCountdown, 1000); // Update every second
 
 function updateCountdown() {
-    var minutes = Math.floor(countdownTime / 60);
-    var seconds = countdownTime % 60;
-
-    countdownElement.innerText = 'Redirecting in ' + minutes + ':' + seconds.toString().padStart(2, '0');
+    countdownTime--;
+    countdownElement.innerText = 'Redirecting in: ' + countdownTime;
 
     if (countdownTime <= 0) {
         clearInterval(countdownInterval);
-        window.location.href = "{% url 'profiles' %}"; // Redirect to the specified URL
-    } else {
-        countdownTime--;
     }
 }
