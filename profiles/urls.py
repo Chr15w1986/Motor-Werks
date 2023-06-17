1ec7c89e44bf4734ce6e05c180f79d1d990739ed
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-17 19:44:03 +0000

    update profile URLS and views

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,7 +7,7 @@ from .views import (ProfileView,
 
 APP_NAME = 'profiles'
 urlpatterns = [
-    path('', ProfileView.as_view(), name='profiles'),
+    path('', ProfileView.as_view(), name='profile'),
     path('update/<int:pk>/', UpdateProfile.as_view(), name='update-profile'),
     path('delete/<int:pk>/', DeleteProfile.as_view(), name='delete-profile'),
     path('delete_success/', DeleteSuccess.as_view(), name='delete-success'),
