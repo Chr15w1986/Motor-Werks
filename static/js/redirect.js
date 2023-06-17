d995a62749127c74b881d50f3b7d6f51fa5d021c
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-17 18:22:55 +0000

    create 5 sec timeout using JS to redirect user to home

---
+++ static/js/redirect.js
@@ -0,0 +1,6 @@
+// Simple timed redirect if the user does not click the home button
+// Set to 5 seconds
+
+setTimeout(function() {
+    window.location.href = "/";
+}, 5000); // Redirect after 5 seconds (5000 milliseconds)
