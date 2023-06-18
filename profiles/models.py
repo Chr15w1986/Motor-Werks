ad25569ad3da7bea99a6e3e608b294551ebc4c1c
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-18 17:26:24 +0000

    remove if not statement for create profile

--- profiles/models.py
+++ profiles/models.py
@@ -26,5 +26,5 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    if created and not hasattr(instance, 'userprofile'):
+    if created:
         UserProfile.objects.create(user=instance)
