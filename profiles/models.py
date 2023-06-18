4854dddbd5ae312f87b16a79bf05bf484f49fd6f
Chr15w1986 <chr15w1986@hotmail.co.uk>
2023-06-18 19:02:56 +0000

    fix bug for profile not showing up

--- profiles/models.py
+++ profiles/models.py
@@ -9,16 +9,16 @@ from django.dispatch import receiver
 class UserProfile(models.Model):
     """ User profile models """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=25, blank=False, default=None)
-    last_name = models.CharField(max_length=25, blank=False, default=None)
-    email = models.CharField(max_length=40, blank=False, default=None)
-    street_address1 = models.CharField(max_length=60, blank=False, default=None)  # noqa
-    street_address2 = models.CharField(max_length=60, blank=True, default=None)
-    town_or_city = models.CharField(max_length=40, blank=False, default=None)
-    county = models.CharField(max_length=40, blank=False, default=None)
-    postcode = models.CharField(max_length=20, blank=False, default=None)
-    phone_number = models.CharField(max_length=20, blank=False, default=None)
-    vehicle_reg = models.CharField(max_length=8, blank=False, default=None)
+    first_name = models.CharField(max_length=25, blank=False)
+    last_name = models.CharField(max_length=25, blank=False)
+    email = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=60, blank=False)  # noqa
+    street_address2 = models.CharField(max_length=60, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    vehicle_reg = models.CharField(max_length=8, blank=False)
 
     def __str__(self):
         return f'{self.user.username}'
